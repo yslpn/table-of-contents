b031c1da211b3ad79ebe10847d88d57e5ed23f31
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-10-23 13:33:52 +0400

    perf: add memo for Header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,9 +1,10 @@
+import { memo } from 'react';
 import { MainLayout } from '../../../shared/ui/main-layout';
 
 import HomeIcon from '../assets/home.svg?react';
 import style from './index.module.css';
 
-export const Header = () => {
+export const Header = memo(() => {
   return (
     <header className={style.header}>
       <MainLayout>
@@ -14,4 +15,6 @@ export const Header = () => {
       </MainLayout>
     </header>
   );
-};
+});
+
+Header.displayName = 'Header';
