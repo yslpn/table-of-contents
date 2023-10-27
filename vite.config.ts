89c04f6c3784ceca260030e78b5626cdf84abccf
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-10-27 16:46:21 +0400

    fix: vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: 'https://yslpn.github.io/table-of-contents/',
+  base: '/table-of-contents/',
 });
