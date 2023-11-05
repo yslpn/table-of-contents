f47d29e08c86bd84a5122d175e0cdb3970aaad51
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-11-06 01:16:30 +0400

    test: add Chromium project to Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,10 +33,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
 
     // {
     //   name: 'firefox',
@@ -63,10 +63,6 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
   ],
 
   /* Run your local dev server before starting the tests */
